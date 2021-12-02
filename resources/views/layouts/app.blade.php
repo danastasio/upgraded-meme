05fb4ab0de726ed7d75e8b39a746a81a5637abee
David Anastasio <david.d.anastasio@uconn.edu>
2021-12-02 13:11:20 -0500

    Add back JS and livewire scripts.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,10 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        @livewireStyles
+
+        <!-- Scripts -->
+        <script src="{{ mix('js/app.js') }}" defer></script>
 
         <!-- Favicon -->
         <link rel="icon" href="{{ asset('favicon.ico') }}?v={{ date('YmdHis') }}">
